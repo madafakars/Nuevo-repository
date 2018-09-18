7dcca3defdb3dd943e57de6d154bda3c8459c0d2
madafacars <rooswall96@hotmail.com>
2018-09-17 21:09:24 -0500

    Segunda version

--- Principal.java
+++ Principal.java
@@ -1,5 +1,6 @@
 public class Principal{
 	public stactic void main(string[] args){
-		system.out.println("Hola Mundo");
+		system.out.println("Segunda Version");
+                system.out.println("Segunda Version");
 }	
 }
